25ef18cb2365a8f7daa6d779c82a8dff64e2f24c
Joao Dias <jpgd94@poli.ufrj.br>
2022-10-18 13:21:08 -0300

    "ultimo commit"

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,16 +1,15 @@
+import { ImgHTMLAttributes } from 'react' // Importando essas tag, eu consigo garantir que as propiedades de Image que vem de gerança estão sendo consideradas
 import styles from './Avatar.module.css'
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{  // Aqui está sendo atribuida a extensão dos atributos para extensao
     hasBorder?: boolean;
-    src: string;
-    alt?: string;
 }
-export function Avatar({hasBorder = true, src, alt }: AvatarProps ) { // Nesse caso estamos definindo valores default tambem
+export function Avatar({hasBorder = true, ...props}: AvatarProps ) { // Nesse caso estamos definindo valores default tambem
     return (
         <img 
         className={ hasBorder ? styles.avatarWithBorder : styles.avatar} // se props.hasBorder for true, faça isso(styles.avatarWithBorder) se falso, faça aquilo(styles.avatar) 
-        src={src} 
-        alt={alt}
+       {...props} // Garantindo que os atributos herdados de ser uma imagem html, possam ser utilizados
+
         />
     )
 }
